e621115375466212abcc18dbd7c91289d5152803
David Molineus <david.molineus@netzmacht.de>
2022-07-14 15:30:59 +0200

    Adopt chances from the contao bundle

--- src/Map/Layer/ProviderLayerHydrator.php
+++ src/Map/Layer/ProviderLayerHydrator.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace Cowegis\Bundle\ContaoProviderLayer\Map\Layer;
 
-use Cowegis\Bundle\Contao\Hydrator\Layer\LayerTypeHydrator;
+use Cowegis\Bundle\Contao\Hydrator\Hydrator;
+use Cowegis\Bundle\Contao\Map\Layer\LayerTypeHydrator;
 use Cowegis\Bundle\Contao\Model\LayerModel;
 use Cowegis\Bundle\Contao\Provider\MapLayerContext;
 use Cowegis\Core\Definition\Layer\Layer;
@@ -26,8 +27,12 @@ final class ProviderLayerHydrator extends LayerTypeHydrator
     /**
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
-    protected function hydrateLayer(LayerModel $layerModel, Layer $layer, MapLayerContext $context): void
-    {
+    protected function hydrateLayer(
+        LayerModel $layerModel,
+        Layer $layer,
+        MapLayerContext $context,
+        Hydrator $hydrator
+    ): void {
         $options = $this->configuration[$layerModel->tile_provider]['options'] ?? [];
         foreach ($options as $target => $source) {
             $layer->options()->set($target, $layerModel->$source);
